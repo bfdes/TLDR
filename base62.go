8eedc498e80eaa11900cc3952f1db979b6341572
Bruno Fernandes <bfdes@users.noreply.github.com>
2020-02-16 17:58:02 +0000

    Sanitise argumentd

--- base62.go
+++ base62.go
@@ -4,32 +4,45 @@ import (
 	"strings"
 )
 
-const characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const base = len(characters)
+// Characters defines the character set for base 62 encoding.
+const Characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+const base = len(Characters)
 
 var digits = make(map[rune]int)
 
 func init() {
-	for i, char := range characters {
+	for i, char := range Characters {
 		digits[char] = i
 	}
 }
 
+// Encode a non-negative integer into a base 62 symbol string.
+// Panics if argument is positive.
 func Encode(id int) string {
+	if id < 0 {
+		panic("Argument must be non-negative")
+	}
 	var sb strings.Builder
 	for id > 0 {
 		rem := id % base
-		sb.WriteByte(characters[rem])
+		sb.WriteByte(Characters[rem])
 		id = id / base
 	}
 	return sb.String()
 }
 
+// Decode a base 62 encoded string fragment.
+// Panics if the fragment contains illegal characters.
 func Decode(fragment string) int {
 	id := 0
 	coeff := 1
 	for _, char := range fragment {
-		id += coeff * digits[char]
+		digit := digits[char]
+		if digits == nil {
+			panic("Fragment contains illegal characters")
+		}
+		id += coeff * digit
 		coeff *= base
 	}
 	return id
