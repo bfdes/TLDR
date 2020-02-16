8692292431dbac65c0db78bffcdaa0573d83f339
Bruno Fernandes <bfdes@users.noreply.github.com>
2020-02-16 23:40:00 +0000

    Regression test

--- base62_test.go
+++ base62_test.go
@@ -5,6 +5,24 @@ import (
 	"testing"
 )
 
+func TestEncode(t *testing.T) {
+	type pair struct {
+		value    int
+		expected string
+	}
+	pairs := []pair{
+		{0, ""},
+		{1, "1"},
+		{62, "01"},
+	}
+	for _, pair := range pairs {
+		actual := Encode(pair.value)
+		if actual != pair.expected {
+			t.Error(pair.value)
+		}
+	}
+}
+
 func TestEncodeDecode(t *testing.T) {
 	testCases := 20
 	for i := 0; i < testCases; i++ {
